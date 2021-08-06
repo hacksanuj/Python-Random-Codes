32590ca4ff04be32167b503bc4d62d4c1288bfea
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-06 08:10:34 +0530

    Add files via upload

---
+++ string/remove space from string.py
@@ -0,0 +1,27 @@
+string = input('Enter a string:: ')
+
+print('\nRemoving spaces from left side ::')
+print('Left trimmed string ::', string.lstrip())
+
+print('\nRemoving spaces from right side ::')
+print('Right trimmed string ::', string.rstrip())
+
+print('\nRemoving spaces from both side ::')
+print('Both side trimmed string ::', string.strip())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
