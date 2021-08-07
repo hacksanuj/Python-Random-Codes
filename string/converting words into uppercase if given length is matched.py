5f879fddb8aac3695be811937189d04c99fadb9c
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-07 14:22:56 +0530

    Add files via upload

---
+++ string/converting words into uppercase if given length is matched.py
@@ -0,0 +1,19 @@
+res_list = []
+
+string = input('Enter the string :: ')
+new_str = string.split()
+
+print('\nThe string list is :: ', new_str)
+
+length = int(input('\nEnter the length to check :: '))
+
+for i in new_str:
+	if len(i) > length:
+		res_list.append(i.upper())
+	else:
+		res_list.append(i)
+
+print('\n final string is :: ',res_list)
+
+
+
