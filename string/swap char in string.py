990e6d9f569fb6b723e1a6808cf556fb69b0abf1
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-13 16:17:41 +0530

    Add files via upload

---
+++ string/swap char in string.py
@@ -0,0 +1,15 @@
+def stringchar(a, b):
+	new_a = b[:-1] + a[-1]
+	new_b = a[:-1] + b[-1]
+
+	return new_a + ' ' + new_b
+
+string_01 = input('Enter the first string :: ')
+string_02 = input('Enter the second string :: ')
+
+str1 = string_01.replace(" ", "")
+str2 = string_02.replace(" ", "")
+
+final_string = stringchar(str1, str2)
+
+print('\nThe final string is :: ',final_string)
