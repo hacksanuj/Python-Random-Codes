b4cc1cfd4b6e90d4a05ef551d47807dee98499b7
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-13 16:19:41 +0530

    Update swap char in string.py

--- string/swap char in string.py
+++ string/swap char in string.py
@@ -1,8 +1,8 @@
 def stringchar(a, b):
 	new_a = b[:-1] + a[-1]
 	new_b = a[:-1] + b[-1]
-
 	return new_a + ' ' + new_b
+
 
 string_01 = input('Enter the first string :: ')
 string_02 = input('Enter the second string :: ')
