5dee06df918ce51fbd442d84e8dc455165b90dc7
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-16 06:02:09 +0530

    Add files via upload

---
+++ dictionary/sun of key value pairs.py
@@ -0,0 +1,20 @@
+dict_raw = {}
+
+val = int(input('How many key/value pairs you want :: '))
+print()
+
+for count, i in enumerate(range(val)):
+	key = int(input(f'Enter the key {count + 1} :: '))
+	value = int(input(f'Enter the value for key {count + 1} :: '))
+	dict_raw.update({key : value})
+	print()
+
+
+print('\nThe dictionary is :: ', dict_raw)
+
+sum_keys = sum(dict_raw.keys())
+sum_values = sum(dict_raw.values())
+
+dict_raw.update({sum_keys : sum_values})
+
+print('\nThe new dictionary is :: ', dict_raw)
