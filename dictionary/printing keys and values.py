b320179ac3b0e3b54e22e0bc42971f2b472c1ef1
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-20 17:23:49 +0530

    Update printing keys and values.py

--- dictionary/printing keys and values.py
+++ dictionary/printing keys and values.py
@@ -9,7 +9,6 @@ for count, i in enumerate(range(val)):
 	dict_raw.update({key : value})
 	print()
 
-
 print('\nThe dictionary is :: ', dict_raw)
 
 print('\n\nIteration of keys :: ')
@@ -29,3 +28,5 @@ print('\nIteration of keys and values together ::')
 for x, y in dict_raw.items():
 	print(f'For key :: {x}', end = ' ')
 	print(f'value is :: {y}')
+	
+	
