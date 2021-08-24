daa4ba2ec09fb7ed58d42bbd964c5a0e2a1fb128
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-24 21:36:46 +0530

    Add files via upload

---
+++ dictionary/square of key as value.py
@@ -0,0 +1,8 @@
+dict_raw = {}
+    
+value = int(input('Enter the value to which you want to generate dictionary :: '))
+
+for i in range(0, value):
+	dict_raw.update({i + 1 : (i + 1) * (i + 1)})
+
+print('\nThe dictionary is :: ', dict_raw)
