22923bdab143fb72de7e9ab8c35803e7fbeafde0
Anuj Gupta <guptaanuj2509@gmail.com>
2021-08-28 06:17:41 +0530

    Add files via upload

---
+++ basics problems/basics operations.py
@@ -0,0 +1,10 @@
+a = int(input("ENter the number A::"))
+b = int(input("ENter the number B::"))
+
+print("Addation::" + str(a + b))
+print("Subtraction::" + str(a - b))
+print("Multipluication::" + str(a * b))
+print("Division::" + str(a / b))
+print("Power::" + str(a ** b))
+print("Floor Division::" + str(a // b))
+print("Modulus::" + str(a % b))
