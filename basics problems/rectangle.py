a654fd13647c925cc4c480e8c0adfcb6195434e9
Anuj Gupta <guptaanuj2509@gmail.com>
2022-01-05 19:31:25 +0530

    Add files via upload

---
+++ basics problems/rectangle.py
@@ -0,0 +1,8 @@
+length = int(input("\nEnter the length::"))
+bre = int(input("ENter the breadth::"))
+
+area = length * bre
+per = 2 * (length + bre)
+
+print("Area of rectangle is ::" + str(area))
+print("perimeter of rectangle is::" + str(per))
