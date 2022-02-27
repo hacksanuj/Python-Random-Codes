fbaa636ff31983f8d9af6392633f5820b97353ea
Anuj Gupta <guptaanuj2509@gmail.com>
2022-02-27 11:14:49 +0530

    Added file

---
+++ List/list methods.py
@@ -0,0 +1,34 @@
+# STRING QUESTION
+
+print('\n**********Using isalnum() function on STRING**********')
+str1 = input('Enter the string :: ')
+print(str1.isalnum())
+
+
+print("\n***********************************************************")
+
+print('**********Using isdigit() function on STRING**********')
+str2 = input('Enter the string :: ')
+print(str2.isdigit())
+
+
+print("\n***********************************************************")
+
+print('**********Using isspace() function on STRING**********')
+str3 = input('Enter the string :: ')
+print(str3.isspace())
+
+
+print("\n***********************************************************")
+
+print('**********Using rindex() function on STRING**********')
+str4 = input('Enter the string :: ')
+search = input('Enter the string you want to search :: ')
+print(str4.rindex(search))
+
+
+print("\n***********************************************************")
+
+print('**********Using lstrip() function on STRING**********')
+str5 = input('Enter the string :: ')
+print(str5.lstrip())
