43f1ab7b863cf6b552607374038534c23a6ddd00
Anuj Gupta <guptaanuj2509@gmail.com>
2022-03-07 21:24:45 +0530

    Updated file contents

--- basics problems/grading_system.py
+++ basics problems/grading_system.py
@@ -18,19 +18,19 @@ elif marks in range(60, 80):
 else:
 	print("Your grade is :: A")
 	
-	
-	
-# marks = int(input('Enter the marks of student :: '))
+# Method without range
 
-# if marks < 25:
-# 	print('Your grade is :: F')
-# elif marks >= 25 and marks <= 45:
-# 	print('Your grade is :: E')
-# elif marks >= 45 and marks <= 50:
-# 	print('Your grade is :: D')
-# elif marks >= 50 and marks <= 60:
-# 	print('Your grade is :: C')
-# elif marks >= 60 and marks <= 80:
-# 	print('Your grade is :: B')
-# else:
-# 	print('Your grade is :: A')
+marks = int(input('Enter the marks of student :: '))
+
+if marks < 25:
+	print('Your grade is :: F')
+elif marks >= 25 and marks <= 45:
+	print('Your grade is :: E')
+elif marks >= 45 and marks <= 50:
+	print('Your grade is :: D')
+elif marks >= 50 and marks <= 60:
+	print('Your grade is :: C')
+elif marks >= 60 and marks <= 80:
+	print('Your grade is :: B')
+else:
+	print('Your grade is :: A')
