8a7e3e7a85fae07deb96363586ab1cb5d084e4fa
Anuj Gupta <hackanuj@protonmail.com>
2022-03-13 16:12:24 +0530

    :art: youngest and oldest age python file

--- basics problems/youngest_and_oldest_person.py
+++ basics problems/youngest_and_oldest_person.py
@@ -14,31 +14,31 @@ print('\nThe minimum age is :: ', min_age)
 
 # simple logic
 
-age_01 = int(input('Enter the age of 1st person :: '))
-age_02 = int(input('\nEnter the age of 2nd person :: '))
-age_03 = int(input('\nEnter the age of 3rd person :: '))
+# age_01 = int(input('Enter the age of 1st person :: '))
+# age_02 = int(input('\nEnter the age of 2nd person :: '))
+# age_03 = int(input('\nEnter the age of 3rd person :: '))
 
 
-if age_01 > age_02 and age_01 > age_03:
-	oldest = age_01
+# if age_01 > age_02 and age_01 > age_03:
+# 	oldest = age_01
 
-elif age_02 > age_01 and age_02 > age_03:
-	oldest = age_02
+# elif age_02 > age_01 and age_02 > age_03:
+# 	oldest = age_02
 
-else:
-	oldest = age_03
+# else:
+# 	oldest = age_03
 
-print('\n\nThe Maximum age is :: ', oldest)
+# print('\n\nThe Maximum age is :: ', oldest)
 
 
-# for youngest age
-if age_01 < age_02 and age_01 < age_03:
-	youngest = age_01
+# # for youngest age
+# if age_01 < age_02 and age_01 < age_03:
+# 	youngest = age_01
 
-elif age_02 < age_01 and age_02 < age_03:
-	youngest = age_02
+# elif age_02 < age_01 and age_02 < age_03:
+# 	youngest = age_02
 
-else:
-	youngest = age_03
+# else:
+# 	youngest = age_03
 
-print('\n\nThe Minimum age is :: ', youngest)
+# print('\n\nThe Minimum age is :: ', youngest)
