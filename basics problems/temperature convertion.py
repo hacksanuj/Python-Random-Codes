10fb0409d3ee34eee09fb5c09d6bf1eca6ee97fa
Anuj Gupta <hackanuj@protonmail.com>
2022-03-29 16:55:23 +0530

    :white_check_mark: Added python file
    Added the python program for temperature convertation

---
+++ basics problems/temperature convertion.py
@@ -0,0 +1,7 @@
+fer = float(input("Enter the temperature in fahernheit::"))
+cel = (fer - 32) * 5 / 9
+print("The temperature in Celsius::",cel)
+
+cel_01 = float(input("\n\nEnter the temperature in Celsius::"))
+fer_01 = (cel_01 * 9/5) + 32
+print("The temperature in fahernheit is::",fer_01)
